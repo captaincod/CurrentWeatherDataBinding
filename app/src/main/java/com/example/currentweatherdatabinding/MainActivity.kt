77013f3cd35a463c5fadcecb571650c62c9b3968
Olesya Ivanova <iod5000@gmail.com>
2022-02-21 21:52:26 +0800

    added binding

--- app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import androidx.databinding.DataBindingUtil
+import com.example.currentweatherdatabinding.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 import java.io.InputStream
 import java.net.URL
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.weather = Weather("Irkutsk", 0.0)
 
         /*
         TODO: реализовать отображение погоды в текстовых полях и картинках
@@ -24,20 +26,28 @@ class MainActivity : AppCompatActivity() {
          */
     }
     suspend fun loadWeather() {
-        val API_KEY = "" // TODO: ключ загрузить из строковых ресурсов
-        // TODO: в строку подставлять API_KEY и город (выбирается из списка или вводится в поле)
-        val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Irkutsk&appid=d6843ab8ee963f5d372296dfff62aed7&units=metric";
-        val stream = URL(weatherURL).getContent() as InputStream
+        val apiKey: String = getString(R.string.apikey)
+        val city: String = "Karluk"
+        val weatherURL =
+            "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric"
+        val stream = URL(weatherURL).content as InputStream
         // JSON отдаётся одной строкой,
         val data = Scanner(stream).nextLine()
         // TODO: предусмотреть обработку ошибок (нет сети, пустой ответ)
         Log.d("mytag", data)
     }
-    public fun onClick(v: View) {
 
+    @DelicateCoroutinesApi
+    fun onClick(view: android.view.View) {
         // Используем IO-диспетчер вместо Main (основного потока)
         GlobalScope.launch (Dispatchers.IO) {
             loadWeather()
         }
+
+        binding.weather = Weather("Karluk", 10.0) // change to random value
+        Log.d("mytag", "weather changed to:")
     }
+
 }
+
+data class Weather(val weather_main: String, var main_temp: Double)
