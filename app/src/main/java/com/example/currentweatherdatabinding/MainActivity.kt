67a57f49e1b14e05a520df5e0abe7c5b0dfc0a76
Olesya Ivanova <iod5000@gmail.com>
2022-02-22 00:47:12 +0800

    working with 1 city

--- app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.currentweatherdatabinding.databinding.ActivityMainBinding
+import com.google.gson.Gson
 import kotlinx.coroutines.*
 import java.io.InputStream
 import java.net.URL
@@ -13,11 +14,12 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.weather = Weather("Irkutsk", 0.0)
+        binding.weather = Weather("City name", "Weather", 0.0)
 
         /*
         TODO: реализовать отображение погоды в текстовых полях и картинках
@@ -25,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         использовать data binding, библиотеки уже подключены
          */
     }
-    suspend fun loadWeather() {
+    suspend fun loadWeather(): WeatherJSON {
         val apiKey: String = getString(R.string.apikey)
         val city: String = "Karluk"
         val weatherURL =
@@ -33,21 +35,41 @@ class MainActivity : AppCompatActivity() {
         val stream = URL(weatherURL).content as InputStream
         // JSON отдаётся одной строкой,
         val data = Scanner(stream).nextLine()
+        Log.d("mytag", "old: $data")
         // TODO: предусмотреть обработку ошибок (нет сети, пустой ответ)
-        Log.d("mytag", data)
+        val jsondata = data.trimIndent()
+        val newdata: WeatherJSON = Gson().fromJson<WeatherJSON>(jsondata, WeatherJSON::class.java)
+        Log.d("mytag", "data: $newdata")
+        return newdata
     }
 
     @DelicateCoroutinesApi
     fun onClick(view: android.view.View) {
         // Используем IO-диспетчер вместо Main (основного потока)
         GlobalScope.launch (Dispatchers.IO) {
-            loadWeather()
+            val weatherData: WeatherJSON = loadWeather()
+            val cityName: String = weatherData.name
+            val weatherMain: String = weatherData.weather[0].main
+            val mainTemp: Double = weatherData.main.temp
+            Log.d("mytag", "cityName: $cityName, weatherMain: $weatherMain, mainTemp: $mainTemp")
+            binding.weather = Weather(cityName, weatherMain, mainTemp)
         }
-
-        binding.weather = Weather("Karluk", 10.0) // change to random value
-        Log.d("mytag", "weather changed to:")
     }
 
 }
 
-data class Weather(val weather_main: String, var main_temp: Double)
+data class Weather(val city_name: String, val weather_main: String, var main_temp: Double)
+
+data class WeatherJSON(val coord: Coord, val weather: Array<WeatherArray>, val base: String,
+                       val main: WeatherMain, val visibility: Long, val wind: WeatherWind,
+                       val clouds: WeatherClouds, val dt: Long, val sys: WeatherSys,
+                       val timezone: Long, val id: Long, val name: String, val cod: Int)
+
+data class Coord(val lon: Double, val lat: Double)
+data class WeatherArray(val id: Int, val main: String, val description: String, val icon: String)
+data class WeatherMain(val temp: Double, val feels_like: Double,
+                       val temp_min: Double, val temp_max: Double,
+                       val pressure: Int, val humidity: Int)
+data class WeatherWind(val speed: Int, val deg: Int)
+data class WeatherClouds(val all: Int)
+data class WeatherSys(val type: Int, val id: Int, val country: String, val sunrise: Long, val sunset: Long)
